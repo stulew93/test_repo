e86a54b9db5d792ea01861aaa69731f9d5f2238d
Stuart Lewis <75398266+stulew93@users.noreply.github.com>
2022-06-22 12:20:32 +0100

    Create access.tf

---
+++ definitions/datasets/example_dataset/access.tf
@@ -0,0 +1,24 @@
+# This file is the only file that needs editing for access to the dataset in this folder
+
+locals {
+  # Define the reader group access
+  reader_group_by_email = [
+  ]
+
+  # Define the individual reader access
+  reader_user_by_email = [
+  ]
+
+  # Define individual owner access
+  owner_user_by_email = [
+  ]
+
+  # Define individual write access
+  writer_user_by_email = [
+  ]
+
+  # Define authorised view access, this should be in the format "project.dataset.view"
+  authorised_view = [
+  ]
+
+}
